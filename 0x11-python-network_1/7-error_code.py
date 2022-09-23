944a921d32789290d8390a410c4e6e4844f49db0
SamDeiSam <pdeisam@gmail.com>
2022-09-23 14:42:54 +0100

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL and
-displays the body of the response"""
+displays the body of the response
+"""
 
+import sys
 import requests
-from sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
